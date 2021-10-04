39cb579391389482f449deb5bece8fbff96483ae
Mehedi Hasan Siam <mehedisiam0022@gmail.com>
2021-10-04 09:35:24 +0600

    update

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -2,8 +2,11 @@ import React from 'react';
 import './NotFound.css'
 const NotFound = () => {
     return (
-        <div>
-            <h1>404</h1>
+        <div className="not-found-container">
+            <div className="not-found-msg">
+                <h1 className="text-center">404 <sup>Oops! You're lost.</sup></h1>
+                <p className="text-center">The page you are looking for was not found</p>
+            </div>
         </div>
     );
 };
